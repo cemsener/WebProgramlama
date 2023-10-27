f95f1f508ae0dd73f52f4ce9b4f10321bc3518b8
cemsener <cemsener_@hotmail.com>
2023-10-27 14:39:05 +0300

    Add files via upload

---
+++ Porto.UI/wwwroot/Content/AngularJs/BaseCtrl.js
@@ -0,0 +1,29 @@
+﻿var app = angular.module('PortoApp', []);
+
+app.controller('BaseCtrl', function ($scope, $http) {
+    $scope.AllCategoryList = [];
+
+    $scope.PageLoad = function () {
+        $scope.GetCategory();
+    }
+
+    $scope.GetCategory = function () {
+        //spinner'ı baslat
+        $http.get("/Category/GetCategory")
+            .then(function (response) {
+                $scope.AllCategoryList = response.data;
+                $scope.ParentCategoryList = $scope.AllCategoryList.filter(x => x.ParentID == null);
+                //spinner'ı kapat
+            })
+            .catch(function (response) {
+                alert(response.message);
+            })
+    }
+
+    $scope.GetSubCategory = function (ParentID) {
+        var subCategories = $scope.AllCategoryList.filter(x => x.ParentID == ParentID);
+        return subCategories;
+    }
+
+    $scope.PageLoad();
+});
